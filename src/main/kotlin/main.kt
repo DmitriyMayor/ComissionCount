cdab7be88ca1271353f90482c36c70cbbe384aff
dmitriymayorov <dmitriy85mayor@gmail.com>
2024-03-03 17:01:33 +0300

    monthTransactionsAmount перенесен в параметры функции, добавлены значения по умолчанию для commissionCount, исправлено условие в mastercardCommissionCount

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,49 @@
 fun main() {
 
     var commission = 0.0
-    val monthTransactionsAmount = 100_000.00
     val transactionAmount = readln().toDouble()
-    val paymentSystemType = "Visa"
+    val paymentSystemType = "Mastercard"
+
 
     fun visaCommissionCount(transactionAmount: Double, minCommission: Double = 35.00): Double {
 
         commission = transactionAmount * 0.075
-        if (commission < 35.00) {
+        if (commission < minCommission) {
             commission = minCommission
         }
         return commission
     }
 
-    fun mastercardCommissionCount(transactionAmount: Double, noCommissionLimit: Double = 75_000.0): Double {
+    fun mastercardCommissionCount(
+        transactionAmount: Double,
+        noCommissionLimit: Double = 75_000.0,
+        monthTransactionsAmount: Double = 100_000.0
+    ): Double {
 
         if (monthTransactionsAmount > noCommissionLimit) {
             commission = transactionAmount * 0.06 + 20
         }
-        commission = if ((monthTransactionsAmount + transactionAmount > noCommissionLimit) && (monthTransactionsAmount < noCommissionLimit)) {
-            (monthTransactionsAmount + transactionAmount - 75_000) * 0.06 + 20
-        } else 0.0
+        commission =
+            if ((monthTransactionsAmount + transactionAmount > noCommissionLimit) || (monthTransactionsAmount < noCommissionLimit)) {
+                (monthTransactionsAmount + transactionAmount - noCommissionLimit) * 0.06 + 20
+            } else 0.0
 
         return commission
     }
 
-    fun commissionCount(paymentSystemType: String, monthTransactionsAmount: Double, transactionAmount: Double): Double {
+    fun commissionCount(
+        paymentSystemType: String,
+        transactionAmount: Double,
+        monthTransactionsAmount: Double = 100_000.0,
+        noCommissionLimit: Double = 75_000.0,
+    ): Double {
 
         if ((transactionAmount <= 150_000.00) && ((monthTransactionsAmount + transactionAmount) <= 600_000)) {
             when (paymentSystemType) {
                 "Visa" -> commission = visaCommissionCount(transactionAmount)
-                "Mastercard" -> commission = mastercardCommissionCount(transactionAmount)
+                "Mastercard" -> commission =
+                    mastercardCommissionCount(transactionAmount, noCommissionLimit, monthTransactionsAmount)
+
                 "Мир" -> commission = 0.0
             }
             println(commission)
@@ -42,7 +54,8 @@ fun main() {
 
         return commission
     }
-    commissionCount(paymentSystemType, monthTransactionsAmount, transactionAmount)
+
+    commissionCount(paymentSystemType, 100_000.0, transactionAmount)
 }
 
 
